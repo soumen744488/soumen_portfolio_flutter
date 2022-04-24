d4fe9a56d57670f488ad2d32114d744942b5740e
SOUMEN PAL <47673411+soumen744488@users.noreply.github.com>
2022-04-24 21:38:04 +0530

    Update Variable.dart
    personal details update

--- lib/Variable.dart
+++ lib/Variable.dart
@@ -24,27 +24,28 @@ const String WHO_AM_I ="Hello! I'm Soumen Pal  , a graduated Computer Science an
     'I love to create performant and interesting stuff that are benificial to the community. I enjoy learning and exploring new areas in the technologies. \nIn my opinion,'
     'getting your hands dirty is the best way to learn a new skill.\n'
     'My area of interest are Cross Platform Application Development , Android Application Development and Full Stack Development\n'
-    'Beside Programming, I love playing cricket and watching cricket matches with a mug full of coffee.';
+    'Beside Programming, I love cooking and bike rideing in free time like to try new os and technology .';
 
 //EDUCATION
 const String DEGREE = "Bachelor of Technology in Computer Science & Engineering ( 2017-2021 ) ";
-const String COLLEGE_NAME = "Saroj Mohan Institute Of Technology";
+const String COLLEGE_NAME = "Saroj Mohan Institute Of Technology(Hooghly,W.B)";
 const String CGPA = "DGPA :- 7.87";
 
-const String TWELVETH_SCHOOL = "HOOMGARH CHANDABILA HIGH SCHOOL";
+const String TWELVETH_SCHOOL = "HOOMGARH CHANDABILA HIGH SCHOOL(WBCHSE)";
 const String TWELVETH_PERSENTAGE = "Percentage :- 64";
 
-const String TENTH_SCHOOL = "AGARBANDH HIGH SCHOOL";
+const String TENTH_SCHOOL = "AGARBANDH HIGH SCHOOL(WBBSE)";
 const String TENTH_PERSENTAGE = "Percentage :- 60";
 
 
 //SKILLS
-const String SKILL1 = "JAVA ( Programming & Android )";
+const String SKILL1 = "JAVA, KOTLIN ( Programming & Android )";
 const String SKILL2 = "PYTHON ( Programming )";
 const String SKILL3 = "FLUTTER ( DART )" ;
 const String SKILL4 = "GIT" ;
 const String SKILL5 = "SQLite" ;
 const String SKILL6 = "FIREBASE";
+
 
 
 
